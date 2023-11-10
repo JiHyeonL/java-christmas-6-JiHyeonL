8ad59e15ec40c293f6ef294725f81eb7eea42647
lee jihyeon <lejinhy@gmail.com>
2023-11-10 22:58:24 +0900

    feat: DiscountEvent 변수로 이벤트 이름 추가

--- src/main/java/christmas/constant/DiscountEvent.java
+++ src/main/java/christmas/constant/DiscountEvent.java
@@ -3,24 +3,30 @@ package christmas.constant;
 import java.time.LocalDate;
 
 public enum DiscountEvent {
-    CHRISTMAS_D_DAY(LocalDate.of(2023, 12, 1),
+    CHRISTMAS_D_DAY("크리스마스 디데이 할인",
+            LocalDate.of(2023, 12, 1),
             LocalDate.of(2023, 12, 25),
             1000),
-    WEEKDAY(LocalDate.of(2023, 12, 1),
+    WEEKDAY("평일 할인",
+            LocalDate.of(2023, 12, 1),
             LocalDate.of(2023, 12, 31),
             2023),
-    WEEKEND(LocalDate.of(2023, 12, 1),
+    WEEKEND("주말 할인",
+            LocalDate.of(2023, 12, 1),
             LocalDate.of(2023, 12, 31),
             2023),
-    SPECIAL(LocalDate.of(2023, 12, 1),
+    SPECIAL("특별 할인",
+            LocalDate.of(2023, 12, 1),
             LocalDate.of(2023, 12, 31),
             1000);
 
+    private final String name;
     private final LocalDate startDate;
     private final LocalDate endDate;
     private final int discountCost;
 
-    DiscountEvent(LocalDate startDate, LocalDate endDate, int discountCost) {
+    DiscountEvent(String name, LocalDate startDate, LocalDate endDate, int discountCost) {
+        this.name = name;
         this.startDate = startDate;
         this.endDate = endDate;
         this.discountCost = discountCost;
