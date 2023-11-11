53b69eed88d70d1ec18b43c55c34fd8eaed842cc
lee jihyeon <lejinhy@gmail.com>
2023-11-11 13:04:48 +0900

    feat: 플래너메세지 프린트 메소드 추가

--- src/main/java/christmas/constant/PlannerMessage.java
+++ src/main/java/christmas/constant/PlannerMessage.java
@@ -3,7 +3,7 @@ package christmas.constant;
 public enum PlannerMessage {
     ANNOUNCE_WELCOME("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     REQUEST_VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
-    REQUSET_MENU_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    REQUEST_MENU_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
     ANNOUNCE_EVENT("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     TITLE_MENU_ORDER("<주문 메뉴>"),
     TITLE_BEFORE_DISCOUNT_AMOUNT("<할인 전 총주문 금액>"),
@@ -21,6 +21,10 @@ public enum PlannerMessage {
 
     PlannerMessage(String message) {
         this.message = message;
+    }
+
+    public void printMessage() {
+        System.out.println(message);
     }
 
 }
