5c666fe99e3dbc308840871bc098b17818fbc505
lee jihyeon <lejinhy@gmail.com>
2023-11-13 16:16:38 +0900

    feat: 증정 이벤트 enum 생성

---
+++ src/main/java/christmas/constant/GiveawayEvent.java
@@ -0,0 +1,23 @@
+package christmas.constant;
+
+import java.time.LocalDate;
+
+public enum GiveawayEvent {
+    CHAMPAGNE("샴페인",
+            LocalDate.of(2023, 12, 1),
+            LocalDate.of(2023, 12, 25),
+            120000);
+
+    private final String gift;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final int minimumOrderAmount;
+
+    GiveawayEvent(String gift, LocalDate startDate, LocalDate endDate, int minimumOrderAmount) {
+        this.gift = gift;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.minimumOrderAmount = minimumOrderAmount;
+    }
+
+}
