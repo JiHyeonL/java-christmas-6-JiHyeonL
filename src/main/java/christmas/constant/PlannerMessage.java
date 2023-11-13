87e218aa05d92eb226a27bb8c0b9f27bf7cd19e2
lee jihyeon <lejinhy@gmail.com>
2023-11-13 17:01:33 +0900

    feat: 출력 타이틀에 모두 \n 추가

--- src/main/java/christmas/constant/PlannerMessage.java
+++ src/main/java/christmas/constant/PlannerMessage.java
@@ -4,14 +4,14 @@ public enum PlannerMessage {
     ANNOUNCE_WELCOME("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     REQUEST_VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
     REQUEST_MENU_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
-    ANNOUNCE_EVENT("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
-    TITLE_MENU_ORDER("<주문 메뉴>"),
-    TITLE_BEFORE_DISCOUNT_AMOUNT("<할인 전 총주문 금액>"),
-    TITLE_GIVEAWAY_MENU("<증정 메뉴>"),
-    TITLE_BENEFIT_DETAIL("<혜택 내역>"),
-    TITLE_BENEFIT_AMOUNT("<총혜택 금액>"),
-    TITLE_EXPECTED_PAYMENT("<할인 후 예상 결제 금액>"),
-    TITLE_EVENT_BADGE("<12월 이벤트 배지>"),
+    ANNOUNCE_EVENT_BENEFIT("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    TITLE_MENU_ORDER("\n<주문 메뉴>"),
+    TITLE_BEFORE_DISCOUNT_AMOUNT("\n<할인 전 총주문 금액>"),
+    TITLE_GIVEAWAY_MENU("\n<증정 메뉴>"),
+    TITLE_BENEFIT_DETAIL("\n<혜택 내역>"),
+    TITLE_BENEFIT_AMOUNT("\n<총혜택 금액>"),
+    TITLE_EXPECTED_PAYMENT("\n<할인 후 예상 결제 금액>"),
+    TITLE_EVENT_BADGE("\n<12월 이벤트 배지>"),
     OUTPUT_ORDER("%s %d개"),
     OUTPUT_PRICE("%,d원"),
     OUTPUT_EVENT_DETAIL("%s: %d원");
@@ -22,8 +22,8 @@ public enum PlannerMessage {
         this.message = message;
     }
 
-    public void printOrder(String menu, int count) {
-        System.out.println(String.format(OUTPUT_ORDER.message, menu, count));
+    public static void printOrderMenu(Menu menu, int count) {
+        System.out.println(String.format(OUTPUT_ORDER.message, menu.getName(), count));
     }
 
     public void printPrice(int price) {
