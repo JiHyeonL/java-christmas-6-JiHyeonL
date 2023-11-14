54172106ac49ee4ceb5a438d2b9611cd6dbba138
lee jihyeon <lejinhy@gmail.com>
2023-11-15 04:44:47 +0900

    fix(GiveawayEvent): 양수 크기비교를 음수 크기비교로 수정

--- src/main/java/christmas/constant/GiveawayEvent.java
+++ src/main/java/christmas/constant/GiveawayEvent.java
@@ -3,31 +3,31 @@ package christmas.constant;
 import java.time.LocalDate;
 
 public enum GiveawayEvent {
-    CHAMPAGNE("샴페인",
+    CHAMPAGNE(Menu.CHAMPAGNE,
             LocalDate.of(2023, 12, 1),
             LocalDate.of(2023, 12, 25),
             120000);
 
-    private final String gift;
+    private final Menu gift;
     private final LocalDate startDate;
     private final LocalDate endDate;
     private final int minimumOrderAmount;
 
-    GiveawayEvent(String gift, LocalDate startDate, LocalDate endDate, int minimumOrderAmount) {
+    GiveawayEvent(Menu gift, LocalDate startDate, LocalDate endDate, int minimumOrderAmount) {
         this.gift = gift;
         this.startDate = startDate;
         this.endDate = endDate;
         this.minimumOrderAmount = minimumOrderAmount;
     }
 
-    public boolean canReceiveGift(int beforeDiscountAmount) {
+    public int calculateGiveawayAmount(int beforeDiscountAmount) {
         if (beforeDiscountAmount >= minimumOrderAmount) {
-            return true;
+            return -gift.getPrice();
         }
-        return false;
+        return 0;
     }
 
-    public String getGift() {
+    public Menu getGift() {
         return gift;
     }
 }
