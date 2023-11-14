fde268ccfab7933d450e3414a292675aa46f0111
lee jihyeon <lejinhy@gmail.com>
2023-11-15 04:50:07 +0900

    feat: 총혜택, 할인금액 계산 메소드 추가

--- src/main/java/christmas/dto/DiscountDetails.java
+++ src/main/java/christmas/dto/DiscountDetails.java
@@ -1,6 +1,7 @@
 package christmas.dto;
 
 import christmas.constant.DiscountEvent;
+import christmas.constant.PlannerMessage;
 
 import java.util.Map;
 
@@ -11,11 +12,41 @@ public class DiscountDetails {
         this.discountAmount = discountAmount;
     }
 
-    public void addDiscountEvent(DiscountEvent event, int price) {
-        discountAmount.put(event, price);
-    }
-
     public int findDiscountPriceByEvent(DiscountEvent event) {
         return discountAmount.get(event);
+    }
+
+    public String makeEventBenefitDetail() {
+        StringBuilder eventFormat = new StringBuilder();
+        for (Map.Entry<DiscountEvent, Integer> event : discountAmount.entrySet()) {
+            if (event.getValue() != 0) {
+                eventFormat.append(PlannerMessage.makeEventBenefitFormat(event.getKey(), event.getValue()))
+                        .append("\n");
+            }
+        }
+        if (eventFormat.length() == 0) {
+            return PlannerMessage.OUTPUT_NO_EVENT.getMessage();
+        }
+
+        eventFormat.deleteCharAt(eventFormat.length() - 1);
+        return eventFormat.toString();
+    }
+
+    public int calculateTotalBenefit() {
+        int total = 0;
+        for (int price : discountAmount.values()) {
+            total += price;
+        }
+        return total;
+    }
+
+    public int calculateBenefitForExpectedPayment() {
+        int total = 0;
+        for (Map.Entry<DiscountEvent, Integer> event : discountAmount.entrySet()) {
+            if (!event.getKey().equals(DiscountEvent.GIVEAWAY)) {
+                total += event.getValue();
+            }
+        }
+        return total;
     }
 }
