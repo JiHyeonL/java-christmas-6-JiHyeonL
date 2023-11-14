b37ad24cd71b43855fa113425f65b7fcc714da35
lee jihyeon <lejinhy@gmail.com>
2023-11-15 01:01:00 +0900

    feat: 증정이벤트도 enum으로 추가

--- src/main/java/christmas/constant/DiscountEvent.java
+++ src/main/java/christmas/constant/DiscountEvent.java
@@ -1,35 +1,103 @@
 package christmas.constant;
 
+import christmas.dto.CalculateEventDto;
+
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
 
 public enum DiscountEvent {
     CHRISTMAS_D_DAY("크리스마스 디데이 할인",
             LocalDate.of(2023, 12, 1),
-            LocalDate.of(2023, 12, 25),
-            1000),
+            LocalDate.of(2023, 12, 25)) {
+        @Override
+        public int calculateDiscountAmount(CalculateEventDto eventParameter) {
+            if (isCanDiscountChristmasEvent(eventParameter.getDate())) {
+                return -1000 + -100 * (eventParameter.getDate() - 1);
+            }
+            return 0;
+        }
+        private boolean isCanDiscountChristmasEvent(int date) {
+            return date <= CHRISTMAS_D_DAY.endDate.getDayOfMonth();
+        }
+    },
+
     WEEKDAY("평일 할인",
             LocalDate.of(2023, 12, 1),
-            LocalDate.of(2023, 12, 31),
-            2023),
+            LocalDate.of(2023, 12, 31)) {
+        @Override
+        public int calculateDiscountAmount(CalculateEventDto eventParameter) {
+            if (isCanDiscountWeekdayEvent(eventParameter.getDate()))
+                return eventParameter.getDessertCount() * -2023;
+            return 0;
+        }
+        private boolean isCanDiscountWeekdayEvent(int date) {
+            LocalDate specificDate = LocalDate.of(2023, 12, date);
+            DayOfWeek dayOfWeek = specificDate.getDayOfWeek();
+            if (dayOfWeek.compareTo(DayOfWeek.THURSDAY) <= 0) {
+                return true;
+            }
+            return dayOfWeek == DayOfWeek.SUNDAY;
+        }
+    },
+
     WEEKEND("주말 할인",
             LocalDate.of(2023, 12, 1),
-            LocalDate.of(2023, 12, 31),
-            2023),
+            LocalDate.of(2023, 12, 31)) {
+        @Override
+        public int calculateDiscountAmount(CalculateEventDto eventParameter) {
+            if (isCanDiscountWeekendEvent(eventParameter.getDate())) {
+                return eventParameter.getMainCount() * -2023;
+            }
+            return 0;
+        }
+        private boolean isCanDiscountWeekendEvent(int date) {
+            LocalDate specificDate = LocalDate.of(2023, 12, date);
+            DayOfWeek dayOfWeek = specificDate.getDayOfWeek();
+            if (dayOfWeek == DayOfWeek.FRIDAY) {
+                return true;
+            }
+            if (dayOfWeek == DayOfWeek.SATURDAY) {
+                return true;
+            }
+            return false;
+        }
+    },
     SPECIAL("특별 할인",
             LocalDate.of(2023, 12, 1),
-            LocalDate.of(2023, 12, 31),
-            1000);
+            LocalDate.of(2023, 12, 31)) {
+        @Override
+        public int calculateDiscountAmount(CalculateEventDto eventParameter) {
+            if (isCanDiscountSpecialEvent(eventParameter.getDate())) {
+                return -1000;
+            }
+            return 0;
+        }
+        private boolean isCanDiscountSpecialEvent(int date) {
+            List<Integer> starDate = List.of(10, 17, 24, 25, 31);
+            if (starDate.contains(date)) {
+                return true;
+            }
+            return false;
+        }
+    },
+   GIVEAWAY("증정 이벤트",
+           LocalDate.of(2023, 12, 1),
+           LocalDate.of(2023, 12, 31)) {
+       @Override
+       public int calculateDiscountAmount(CalculateEventDto eventParameter) {
+           return GiveawayEvent.CHAMPAGNE.calculateGiveawayAmount(eventParameter.getBeforeDiscountAmount());
+       }
+   };
 
     private final String name;
     private final LocalDate startDate;
     private final LocalDate endDate;
-    private final int discountCost;
-
-    DiscountEvent(String name, LocalDate startDate, LocalDate endDate, int discountCost) {
+    DiscountEvent(String name, LocalDate startDate, LocalDate endDate) {
         this.name = name;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.discountCost = discountCost;
     }
 
+    public abstract int calculateDiscountAmount(CalculateEventDto eventParameter);
 }
