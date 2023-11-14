811947baa9341f771caa28fdf5c4d2d7ccd53ef5
lee jihyeon <lejinhy@gmail.com>
2023-11-15 04:43:59 +0900

    feat: 뱃지 결과를 반환하는 메소드 추가

--- src/main/java/christmas/constant/EventBadge.java
+++ src/main/java/christmas/constant/EventBadge.java
@@ -1,9 +1,10 @@
 package christmas.constant;
 
 public enum EventBadge {
-    STAR("별", 5000),
-    TREE("트리", 10000),
-    SANTA("산타", 20000);
+    SANTA("산타", -20000),
+    TREE("트리", -10000),
+    STAR("별", -5000);
+
 
     private final String name;
     private final int cost;
@@ -11,5 +12,14 @@ public enum EventBadge {
     EventBadge(String name, int cost) {
         this.name = name;
         this.cost = cost;
+    }
+
+    public static String chooseBadgeNameByBenefit(int totalBenefit) {
+        for (EventBadge badge : EventBadge.values()) {
+            if (totalBenefit < badge.cost) {
+                return badge.name;
+            }
+        }
+        return PlannerMessage.OUTPUT_NO_EVENT.getMessage();
     }
 }
