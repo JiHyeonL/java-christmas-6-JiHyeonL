10ff2a74871c42f3984675004a6443e60aa522eb
lee jihyeon <lejinhy@gmail.com>
2023-11-15 16:01:35 +0900

    test: 총혜택 가격에 따른 배지 반환결과 테스트 완료

---
+++ src/test/java/christmas/constant/EventBadgeTest.java
@@ -0,0 +1,28 @@
+package christmas.constant;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventBadgeTest {
+    private EventBadge eventBadge;
+
+    @Test
+    void 배지_별_6000원일때() {
+        String badge = eventBadge.chooseBadgeNameByBenefit(-6000);
+        assertThat(badge).isEqualTo("별");
+    }
+
+    @Test
+    void 배지_트리_11000원일때() {
+        String badge = eventBadge.chooseBadgeNameByBenefit(-11000);
+        assertThat(badge).isEqualTo("트리");
+    }
+
+    @Test
+    void 배지_산타_20000원일때() {
+        String badge = eventBadge.chooseBadgeNameByBenefit(-20000);
+        assertThat(badge).isEqualTo("산타");
+    }
+}
