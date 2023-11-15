3de0b913785a4839985fbd6110fca20c8539c15c
lee jihyeon <lejinhy@gmail.com>
2023-11-15 21:55:48 +0900

    refactor: 사용하지 않는 enum 속성 삭제

--- src/main/java/christmas/constant/GiveawayEvent.java
+++ src/main/java/christmas/constant/GiveawayEvent.java
@@ -1,22 +1,13 @@
 package christmas.constant;
 
-import java.time.LocalDate;
-
 public enum GiveawayEvent {
-    CHAMPAGNE(Menu.CHAMPAGNE,
-            LocalDate.of(2023, 12, 1),
-            LocalDate.of(2023, 12, 25),
-            120000);
+    CHAMPAGNE(Menu.CHAMPAGNE, 120000);
 
     private final Menu gift;
-    private final LocalDate startDate;
-    private final LocalDate endDate;
     private final int minimumOrderAmount;
 
-    GiveawayEvent(Menu gift, LocalDate startDate, LocalDate endDate, int minimumOrderAmount) {
+    GiveawayEvent(Menu gift, int minimumOrderAmount) {
         this.gift = gift;
-        this.startDate = startDate;
-        this.endDate = endDate;
         this.minimumOrderAmount = minimumOrderAmount;
     }
 
@@ -25,9 +16,5 @@ public enum GiveawayEvent {
             return -gift.getPrice();
         }
         return 0;
-    }
-
-    public Menu getGift() {
-        return gift;
     }
 }
