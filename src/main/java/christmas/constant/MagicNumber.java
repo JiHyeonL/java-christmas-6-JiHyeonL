283dc6df748843fd83558e05cef4c22141445d5e
lee jihyeon <lejinhy@gmail.com>
2023-11-15 22:33:59 +0900

    feat: 날짜 범위 enum 추가

--- src/main/java/christmas/constant/MagicNumber.java
+++ src/main/java/christmas/constant/MagicNumber.java
@@ -2,7 +2,10 @@ package christmas.constant;
 
 public enum MagicNumber {
     ORDER_ELEMENT_COUNT(2),
-    MAX_ORDER_COUNT(20);
+    MAX_ORDER_COUNT(20),
+    MIN_DATE(1),
+    MAX_DATE(31),
+    MIN_EVENT_ACTIVE_BENEFIT(10000);
 
     int number;
     MagicNumber(int number) {
