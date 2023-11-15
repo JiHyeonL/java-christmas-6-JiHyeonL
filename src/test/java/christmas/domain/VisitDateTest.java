f78eec9d0d4e342d5c3d3b47ec475666aaa5d841
lee jihyeon <lejinhy@gmail.com>
2023-11-15 20:06:50 +0900

    test: 할인금액 계산 테스트 완료

---
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -0,0 +1,33 @@
+package christmas.domain;
+
+import christmas.constant.DiscountEvent;
+import christmas.dto.CalculateEventDto;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitDateTest {
+    private VisitDate visitDate;
+
+    @Test
+    void 할인금액_계산() {
+        CalculateEventDto eventParameter = new CalculateEventDto(11500);
+        eventParameter.setDessertCount(0);
+        eventParameter.setMainCount(0);
+        visitDate = new VisitDate(21);
+
+        Map<DiscountEvent, Integer> result = visitDate.getCalculatedDiscount(eventParameter);
+        Map<DiscountEvent, Integer> answer = new HashMap<>();
+        answer.put(DiscountEvent.CHRISTMAS_D_DAY, -1000 - 100*20);
+        answer.put(DiscountEvent.WEEKDAY, 0);
+        answer.put(DiscountEvent.WEEKEND, 0);
+        answer.put(DiscountEvent.SPECIAL, 0);
+        answer.put(DiscountEvent.GIVEAWAY, 0);
+
+        assertThat(result).isEqualTo(answer);
+    }
+}
